093fc0590d9de1c6be49503cb9298dc3eac24e0f
mohammad-owais-ra <159616127+mohammad-owais-ra@users.noreply.github.com>
2024-03-27 07:25:44 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    message: "Please select a number between 1-10",
+    type: "Number",
+    name: "userGuessedNumber",
+  },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("Congratulation You Guessed A Correct Number");
+} else {
+  console.log("Oh no You Guessed Wrong Number");
+}
+if (answer.userGuessedNumber !== randomNumber) {
+  console.log("The answer was", randomNumber);
+}
